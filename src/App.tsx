6c22698ca43cb80ddc4ea856646266a1c0d2a51c
Nadav Sharkany <nadav@tippingpointc.com>
2026-01-18 15:19:52 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div className="w-screen h-screen overflow-hidden">
+      <iframe
+        src="https://schedule.euro-asia.co.il/business-page/69132a61e469ff9322ebbb73"
+        className="w-full h-full border-0"
+        title="Schedule"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+    </div>
+  );
+}
